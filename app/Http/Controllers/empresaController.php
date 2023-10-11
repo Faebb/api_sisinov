5d17dd32cc55a547a3e6b50c18cff7b9f421afdd
Faebb <faeb265@gmail.com>
2023-10-11 11:14:09 -0500

    read empresa

--- app/Http/Controllers/empresaController.php
+++ app/Http/Controllers/empresaController.php
@@ -4,17 +4,56 @@ namespace App\Http\Controllers;
 
 use App\Models\Empresa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class empresaController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return Empresa::all();
-    }
+        try {
+            $empresas = Empresa::select(
+                'empresa.id_e',
+                'empresa.Nit_E',
+                'empresa.Nom_E',
+                'empresa.Eml_E',
+                'empresa.Nom_Rl',
+                'tipo_doc.N_TDoc as ID_Doc',
+                'empresa.CC_Rl',
+                'empresa.telefonoGeneral',
+                'empresa.Val_E',
+                DB::raw("CASE WHEN empresa.Est_E = 1 THEN 'Activo' ELSE 'Inactivo' END as Est_E"),
+                'empresa.Fh_Afi',
+                'empresa.fechaFinalizacion',
+                'empresa.COD_SE',
+                'empresa.COD_AE'
+            )
+                ->join('tipo_doc', 'empresa.ID_Doc', '=', 'tipo_doc.ID_Doc')
+                ->get();
 
+            if ($empresas->isEmpty()) {
+                return [
+                    'status' => 'error',
+                    'message' => 'No se encontraron Empresas',
+                    'data' => [],
+                ];
+            } else {
+                return [
+                    'status' => 'success',
+                    'message' => 'Empresas encontradas correctamente',
+                    'data' => $empresas,
+                ];
+            }
+        } catch (\Exception $e) {
+            return [
+                'status' => 'error',
+                'message' => 'Error al buscar las Empresas: ' . $e->getMessage(),
+                'data' => null,
+            ];
+        }
+    }
     /**
      * Show the form for creating a new resource.
      */
@@ -36,8 +75,49 @@ class empresaController extends Controller
      */
     public function show(Empresa $empresa)
     {
-        //
+        try {
+            $resultado = Empresa::select(
+                'empresa.id_e',
+                'empresa.Nit_E',
+                'empresa.Nom_E',
+                'empresa.Eml_E',
+                'empresa.Nom_Rl',
+                'tipo_doc.N_TDoc as ID_Doc',
+                'empresa.CC_Rl',
+                'empresa.telefonoGeneral',
+                'empresa.Val_E',
+                DB::raw("CASE WHEN empresa.Est_E = 1 THEN 'Activo' ELSE 'Inactivo' END as Est_E"),
+                'empresa.Fh_Afi',
+                'empresa.fechaFinalizacion',
+                'empresa.COD_SE',
+                'empresa.COD_AE'
+            )
+                ->join('tipo_doc', 'empresa.ID_Doc', '=', 'tipo_doc.ID_Doc')
+                ->where('empresa.id_e', $empresa->id_e)
+                ->first();
+
+            if ($resultado) {
+                return [
+                    'status' => 'success',
+                    'message' => 'Empresa encontrada correctamente',
+                    'data' => $resultado,
+                ];
+            } else {
+                return [
+                    'status' => 'error',
+                    'message' => 'Empresa no encontrada',
+                    'data' => null,
+                ];
+            }
+        } catch (\Exception $e) {
+            return [
+                'status' => 'error',
+                'message' => 'Error al buscar la Empresa: ' . $e->getMessage(),
+                'data' => null,
+            ];
+        }
     }
+
 
     /**
      * Show the form for editing the specified resource.
