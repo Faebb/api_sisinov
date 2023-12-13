513c361e1735ef4039b813ede8e2bbc3c43e39bd
Faebb <faeb265@gmail.com>
2023-12-13 14:49:08 -0500

    update novedad

--- app/Http/Controllers/novedadUpdateTpNovedadController.php
+++ app/Http/Controllers/novedadUpdateTpNovedadController.php
@@ -8,35 +8,41 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class novedadUpdateTpNovedadController extends Controller
-{    
-    public function update(Request $request){
-        $datosModel = $request->all();
+{
+    public function update(Request $request)
+    {
+        $data = $request->all();
+        $token = $data['nToken'];
+
+        if (app(tokenController::class)->token($token)) {
+            try {
+                DB::table('novedad')
+                    ->where('ID_Nov', $data["ID_Nov"])
+                    ->update([
+                        'T_Nov' => $data["T_Nov"],
+                        'Des_Nov' => $data["Des_Nov"],
+                        'id_em' => $data["id_em"]
+                    ]);
     
-        
-    
-        
-    
-        try {
-            DB::table('novedad')
-                ->where('ID_Nov', $datosModel["ID_Nov"])
-                ->update([
-                    'T_Nov' => $datosModel["T_Nov"],
-                    'Des_Nov' => $datosModel["Des_Nov"],
-                    'id_em' => $datosModel["id_em"]
-                ]);
-    
-            return response()->json([
-                'error' => false,
-                'status' => 'success',
-                'message' => 'Update successful',
-            ], 200);
-        } catch (\Exception $e) {
+                return response()->json([
+                    'error' => false,
+                    'status' => 'success',
+                    'message' => 'Update successful',
+                ], 200);
+            } catch (\Exception $e) {
+                return response()->json([
+                    'error' => true,
+                    'status' => 'error',
+                    'message' => 'Update failed: ' . $e->getMessage()
+                ], 500);
+            }
+        } else {
             return response()->json([
                 'error' => true,
                 'status' => 'error',
-                'message' => 'Update failed: ' . $e->getMessage()
-            ], 500);
+                'message' => 'No autorizado',
+                'data' => [],
+            ], 401);
         }
     }
-    
 }
