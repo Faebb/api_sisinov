2357138e942ba29ac63be47e1e1205a301ce88b4
Faebb <faeb265@gmail.com>
2023-12-12 23:20:51 -0500

    as

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -2,18 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Token;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class loginController extends Controller
 {
-    public function login()
+    public function login(Request $request)
     {
+        $data = $request->all();
+        $documento = $data['documento'];
+        $passw = $data['passw'];
+        
         try {
-            $datosModel = request()->all();
-            $documento = $datosModel['documento'];
-            $passw = $datosModel['passw'];
-
             $user = DB::table('empleado')
                 ->join('login', 'empleado.id_em', '=', 'login.id_em')
                 ->join('user_rol', 'login.ID_Log', '=', 'user_rol.ID_Log')
@@ -23,13 +24,33 @@ class loginController extends Controller
                 ->where('empleado.estado', 0)
                 ->select('empleado.id_em', 'empleado.n_em', 'rol.ID_rol', 'rol.N_rol')
                 ->first();
+                //guardo id de el empleado
+                $idEmpleado = $user->id_em;
 
             if ($user) {
-                return response()->json([
-                    'error' => false,
-                    'message' => 'Ingreso exitoso al sistema',
-                    'user' => $user,
+                //guardo la fecha actual mas un día
+                $fechaActual = \Carbon\Carbon::now();
+                $fechaMasUnDia = $fechaActual->addDay();
+
+                //genero token
+                $ntoken = $this->generarTokenUnico();
+
+                $token = new Token([
+                    'id_em' => $idEmpleado,
+                    'nToken' => $ntoken,
+                    'fechaVencimiento' => $fechaMasUnDia,
                 ]);
+                $token->save();
+
+                $Tokenf = $token->nToken;
+                if ($Tokenf !== null) {
+                    return response()->json([
+                        'error' => false,
+                        'message' => 'Ingreso exitoso al sistema',
+                        'user' => $user,
+                        'token' => $Tokenf
+                    ]);
+                }
             } else {
                 return response()->json([
                     'error' => true,
@@ -37,11 +58,36 @@ class loginController extends Controller
                 ]);
             }
         } catch (\Exception $e) {
+            // Imprime el mensaje de la excepción para depuración
+            dd($e->getMessage());
+
+            // Devuelve una respuesta JSON con el mensaje de error
             return response()->json([
                 'error' => true,
-                'message' => 'Método de solicitud no válido',
+                'message' => 'Error desconocido: ' . $e->getMessage(),
             ]);
         }
+    }
+
+    private function generarTokenUnico()
+    {
+        $uniqueString = $this->generateUniqueString();
+        return $this->randomCase($uniqueString);
+    }
+
+    private function generateUniqueString($length = 32)
+    {
+        $bytes = random_bytes($length / 2);
+        return bin2hex($bytes);
+    }
+
+    private function randomCase($string)
+    {
+        $result = '';
+        for ($i = 0; $i < strlen($string); $i++) {
+            $result .= (rand(0, 1) == 0) ? strtolower($string[$i]) : strtoupper($string[$i]);
+        }
+        return $result;
     }
 
     public function verifpass()
---
+++ app/Models/Token.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Token
+ * 
+ * @property int $id_token
+ * @property int $id_em
+ * @property string $nToken
+ * @property Carbon $fechaVencimiento
+ * 
+ * @property Token $token
+ *
+ * @package App\Models
+ */
+
+class Token extends Model
+{
+    protected $table = 'Token';
+    protected $primaryKey = 'id_token';
+    public $timestamps = false;
+
+    protected $fillable = [
+        'id_em',
+        'nToken',
+        'fechaVencimiento'
+    ];
+}
